3b978314cb5496d9ad3bc8c5558f445c2c4ca7a9
Sergio <sergi0vargas@users.noreply.github.com>
2020-05-23 16:27:00 -0500

    Agregadas las bases de datos
    Se crearion migracion y modelo para
    las tablas numeros y servidores.

---
+++ app/Numero.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+
+class Numero
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'numero', 'nombre',
+    ];
+
+}
---
+++ app/Server.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+
+class Server
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'ip', 'url',
+    ];
+
+}
